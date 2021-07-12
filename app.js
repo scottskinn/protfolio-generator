604ed50e1d4e36697edde202dcac9273885d37a9
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-07-12 12:01:55 -0400

    HTML file

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
 // console.log(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+//     console.log('================')
+//     // this method is better for arrays
+//     profileDataArr.forEach(profileItem => console.log(profileItem))
+// };
 
-    console.log('================')
-    // this method is better for arrays
-    profileDataArr.forEach(profileItem => console.log(profileItem))
-};
-
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
