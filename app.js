7bf734fcab3433b1a3e62d6cbc7b1cc5a87cf481
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-07-15 17:01:52 -0400

    All done!

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const inquirer = require('inquirer');
 // console.log(inquirer);
-
-const fs = require('fs');
-
+// const fs = require('fs');
+const generateSite = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 
 // const pageHTML = generatePage(mockData);
@@ -153,16 +153,36 @@ Add a new Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-          if (err) throw new Error(err);
-
-          console.log('Portfolio complete! Check out index.html to see the output!');
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
 
+//     fs.writeFile('./dist/index.html', pageHTML, err => {
+//         if (err) throw new Error(err);
 
+//         console.log('Portfolio complete! Check out index.html to see the output!');
+//     });
+
+//     fs.copyFile('./src/style.css', './dist/style.css', err => {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+//         console.log('Style sheet copied successfully!');
+//     });
+// ;
 
 
 
